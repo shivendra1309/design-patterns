9623e49f97bea6e167da4b2b486ce5ad2035179e
shivendra1309 <shivendrapratapsinghchauhan.13@gmail.com>
2023-07-05 17:31:41 +0530

    changed message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         //will make design patterns here
-        System.out.println("Hello world!");
+        System.out.println("let's get started");
     }
 }
