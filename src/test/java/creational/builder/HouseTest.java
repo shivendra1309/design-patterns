063f3260e8911a88a90170a091b6baa13d5a2e16
shivendra1309 <shivendrapratapsinghchauhan.13@gmail.com>
2023-07-11 14:15:13 +0530

    made test class public

--- src/test/java/creational/builder/HouseTest.java
+++ src/test/java/creational/builder/HouseTest.java
@@ -1,10 +1,10 @@
 package creational.builder;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class HouseTest {
+public class HouseTest {
 
     @Test
     public void testHouse(){
