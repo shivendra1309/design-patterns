43f1c18d933ac9a9d1573ed6c3ca3d2b895c71c4
shivendra1309 <shivendrapratapsinghchauhan.13@gmail.com>
2023-07-14 17:11:12 +0530

    added adapter pattern

---
+++ src/main/java/structural/adapter/SocketObjectAdapter.java
@@ -0,0 +1,25 @@
+package structural.adapter;
+
+public class SocketObjectAdapter implements SocketAdapter{
+
+    private Socket socket = new Socket();
+
+    @Override
+    public Volt get120Volt() {
+        return socket.getVolt();
+    }
+
+    @Override
+    public Volt get12Volt() {
+        return convertVolt(socket.getVolt(), 10);
+    }
+
+    @Override
+    public Volt get3Volt() {
+        return convertVolt(socket.getVolt(), 40);
+    }
+
+    private Volt convertVolt(Volt v, int i) {
+        return new Volt(v.getVolts()/i);
+    }
+}
